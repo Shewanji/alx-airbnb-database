933e002b4ef03fbd70122c89b4f08053ae7d5e3e
Shewanji <marrionwanjiru26@gmail.com>
2024-12-09 18:46:06 +0300

    Add window function ROW_NUMBER

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -17,7 +17,7 @@ GROUP BY
 ORDER BY 
     total_bookings DESC;
 
--- Use a window function (ROW_NUMBER, RANK) to rank properties based on the total number of bookings they have received
+-- Use a window function RANK to rank properties based on the total number of bookings they have received
 SELECT 
     p.property_id, 
     p.name, 
@@ -34,3 +34,21 @@ GROUP BY
     p.name
 ORDER BY 
     rank;
+
+-- Use a window function ROW_NUMBER to rank properties based on the total number of bookings they have received
+SELECT 
+    p.property_id, 
+    p.name, 
+    COUNT(b.booking_id) AS total_bookings, 
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS row_number
+FROM 
+    Property p
+LEFT JOIN 
+    Booking b
+ON 
+    p.property_id = b.property_id
+GROUP BY 
+    p.property_id, 
+    p.name
+ORDER BY 
+    row_number;
